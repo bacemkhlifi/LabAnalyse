d9e70c09d2ce62351b6d076e752e2ab6935c2470
bacemkhlifi <bacemkhlifi001@gmail.com>
2021-03-10 22:38:00 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SectionComponent } from './components/section/section.component';
+import { SectionPresentationComponent } from './components/section-presentation/section-presentation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SectionComponent,
+    SectionPresentationComponent
   ],
   imports: [
     BrowserModule
